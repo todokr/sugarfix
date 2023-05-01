ba9647868908c96e21b7af7bab5189a3ef86a87e
todokr <s.tadokoro0317@gmail.com>
2023-05-01 15:48:29 +0900

    impl parser & test

--- src/main/scala/io/github/todokr/sugarfix/dsl/Parser.scala
+++ src/main/scala/io/github/todokr/sugarfix/dsl/Parser.scala
@@ -2,7 +2,7 @@ package io.github.todokr.sugarfix.dsl
 
 import fastparse.Parsed
 import io.github.todokr.sugarfix.{Column, FocalTable, Row, SpecifiedValue}
-
+import io.github.todokr.sugarfix.SpecifiedValue.{Reference, Text}
 
 trait Parser {
   import scala.util.chaining._
@@ -12,10 +12,10 @@ trait Parser {
   def parseDsl(raw: String): Seq[FocalTable] =
     raw.pipe(normalize).pipe(parse)
 
-  private def normalize(input: String): String =
+  private[dsl] def normalize(input: String): String =
     input.stripMargin.linesIterator.map(_.trim).filter(_.nonEmpty).mkString(Newline)
 
-  private def parse(input: String): Seq[FocalTable] =
+  private[dsl] def parse(input: String): Seq[FocalTable] =
     fParse(input, DslExpr(_)) match {
       case Parsed.Success(value, _) => value
       case failure: Parsed.Failure => throw new Exception(failure.toString)
@@ -23,37 +23,41 @@ trait Parser {
 
 }
 
-private object Parser {
+private[dsl] object Parser {
   import fastparse._
   import NoWhitespace._
 
   val Newline: String = "\n"
 
-  def DslExpr[_: P]: P[Seq[FocalTable]] = P(FocalTableExpr.rep)
+  def DslExpr[$: P]: P[Seq[FocalTable]] = P(FocalTableExpr.rep)
 
-  def FocalTableExpr[_: P]: P[FocalTable] =
+  def FocalTableExpr[$: P]: P[FocalTable] =
     P(TableName ~ UpperLine ~ HeaderLine ~ DelimiterLine ~ ContentLines ~ LowerLine).map {
       case (tableName, headers, contents) =>
         val rows = contents.map { values =>
-          val cols = headers.zip(values).map { case (header, value) => Column(header, SpecifiedValue(value)) }
+          val cols = headers.zip(values).map { case (header, value) => Column(header, value) }
           Row(cols)
         }
         FocalTable(tableName, rows)
     }
 
-  def TableName[_: P]: P[String] = P("#" ~ WSs ~ Term.! ~ Newline)
-  def HeaderLine[_: P]: P[Seq[String]] = P(("│" ~ WSs ~ Term.!).rep(1) ~ "│" ~ Newline).map(_.map(_.trim))
-  def ContentLines[_: P]: P[Seq[Seq[String]]] = P(ContentLine.rep(1))
-  def ContentLine[_: P]: P[Seq[String]] = P(("│" ~ WSs ~ Term.!).rep(1) ~ "│" ~ Newline).map(_.map(_.trim))
-  def UpperLine[_: P]: P[Unit] = P("┌" ~ ("─" | "┬").rep ~ "┐" ~ Newline)
-  def DelimiterLine[_: P]: P[Unit] = P("├" ~ ("─" | "┼").rep ~ "┤" ~ Newline)
-  def LowerLine[_: P]: P[Unit] = P("└" ~ ("─" | "┴").rep ~ "┘" ~ (Newline | ""))
+  def TableName[$: P]: P[String] = P("#" ~ WSs ~ Term.! ~ Newline)
+  def HeaderLine[$: P]: P[Seq[String]] = P(("│" ~ WSs ~ Term.!).rep(1) ~ "│" ~ Newline).map(_.map(_.trim))
+  def ContentLines[$: P]: P[Seq[Seq[SpecifiedValue]]] = P((ContentLine ~ Newline.?).rep(1))
+  def ContentLine[$: P]: P[Seq[SpecifiedValue]] = P("│" ~ (WSs ~ (ReferenceTerm | TextTerm) ~ WSs ~ "│").rep)
+  def UpperLine[$: P]: P[Unit] = P("┌" ~ ("─" | "┬").rep ~ "┐" ~ Newline)
+  def DelimiterLine[$: P]: P[Unit] = P("├" ~ ("─" | "┼").rep ~ "┤" ~ Newline)
+  def LowerLine[$: P]: P[Unit] = P("└" ~ ("─" | "┴").rep ~ "┘" ~ (Newline | ""))
 
-  def WS[_: P] = P(" ")
-  def WSs[_: P] = P(WS.rep)
-  def Alpha[_: P] = P(CharIn("A-z").rep(1))
-  def Number[_: P] = P(CharIn("0-9").rep(1))
-  def AlNum[_: P] = P((Number | Alpha).rep(1))
-  def Term[_: P] = P(AlNum ~ (AlNum | WS).rep)
+  def ReferenceTerm[$: P]: P[SpecifiedValue] =
+    P(Token.rep.! ~ WSs ~ "->" ~ WSs ~ Token.rep.!).map { case (t, a) => Reference(t, a) }
+  def TextTerm[$: P]: P[SpecifiedValue] = P(Term.rep.!).map(x => Text(x.trim))
+
+  def WS[$: P] = P(" ")
+  def WSs[$: P] = P(WS.rep)
+  def Alpha[$: P] = P(CharIn("a-z", "A-Z").rep(1))
+  def Number[$: P] = P(CharIn("0-9").rep(1))
+  def Token[$: P] = P((Number | Alpha | "_" | "-").rep(1))
+  def Term[$: P] = P(Token ~ (Token | WS).rep)
 }
 
--- src/test/scala/io/github/todokr/sugarfix/dsl/ParserTest.scala
+++ src/test/scala/io/github/todokr/sugarfix/dsl/ParserTest.scala
@@ -1,5 +1,106 @@
 package io.github.todokr.sugarfix.dsl
 
-class ParserTest {
+import fastparse.{parse => fParse}
+import io.github.todokr.sugarfix.SpecifiedValue._
+import io.github.todokr.sugarfix._
+
+class ParserTest extends munit.FunSuite {
+  val parser = new Parser {}
+
+  test("normalize") {
+    val input =
+      """
+        |
+        |  # departments
+        |  ┌───────┬───────────────────────────┐
+        |  │ alias │      department_name      │
+        |  ├───────┼───────────────────────────┤
+        |  │ hr    │ Human Resource Department │
+        |  │ sales │ Sales Department          │
+        |  └───────┴───────────────────────────┘
+        |
+        |  """
+
+    val result = parser.normalize(input)
+    val expected =
+      """# departments
+        |┌───────┬───────────────────────────┐
+        |│ alias │      department_name      │
+        |├───────┼───────────────────────────┤
+        |│ hr    │ Human Resource Department │
+        |│ sales │ Sales Department          │
+        |└───────┴───────────────────────────┘""".stripMargin
+
+    assertEquals(result, expected)
+  }
+
+  test("parse reference") {
+    val input    = "departments -> hr"
+    val result   = fParse(input, Parser.ReferenceTerm(_)).get.value
+    val expected = Reference("departments", "hr")
+    assertEquals(result, expected)
+  }
+
+  test("parseDsl") {
+    val input =
+      """# departments
+        |┌───────┬───────────────────────────┐
+        |│ alias │      department_name      │
+        |├───────┼───────────────────────────┤
+        |│ hr    │ Human Resource Department │
+        |│ sales │ Sales Department          │
+        |└───────┴───────────────────────────┘
+        |
+        |# users
+        |┌──────────────┬───────────┬─────┬──────────────────────┐
+        |│    alias     │ user_name │ age │    department_id     │
+        |├──────────────┼───────────┼─────┼──────────────────────┤
+        |│ hr_person    │ Jinnai    │  31 │ departments -> hr    │
+        |│ sales_person │ Urita     │  27 │ departments -> sales │
+        |└──────────────┴───────────┴─────┴──────────────────────┘""".stripMargin
+
+    val result = parser.parseDsl(input)
+    val expected = Seq(
+      FocalTable(
+        tableName = "departments",
+        rows = Seq(
+          Row(
+            Seq(
+              Column("alias", Text("hr")),
+              Column("department_name", Text("Human Resource Department"))
+            )
+          ),
+          Row(
+            Seq(
+              Column("alias", Text("sales")),
+              Column("department_name", Text("Sales Department"))
+            )
+          )
+        )
+      ),
+      FocalTable(
+        tableName = "users",
+        rows = Seq(
+          Row(
+            Seq(
+              Column("alias", Text("hr_person")),
+              Column("user_name", Text("Jinnai")),
+              Column("age", Text("31")),
+              Column("department_id", Reference("departments", "hr"))
+            )
+          ),
+          Row(
+            Seq(
+              Column("alias", Text("sales_person")),
+              Column("user_name", Text("Urita")),
+              Column("age", Text("27")),
+              Column("department_id", Reference("departments", "sales"))
+            )
+          )
+        )
+      )
+    )
+    assertEquals(result, expected)
+  }
 
 }
